47563359eb5d63e8b8dbd597b4a80e167d75a05d
jonathanbarzola <jonabl2102@gmail.com>
2022-11-25 09:59:42 -0500

    Angular

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent {
+
+  heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor', 'Capitán América'];
+  heroeBorrado: string = '';
+  heroesBorrados: string[] = [];
+
+  borrarHeroe(): void {
+    this.heroeBorrado = this.heroes.shift() || '';
+  }
+}
